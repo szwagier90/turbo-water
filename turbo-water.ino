cbbb9f16841f0ebc61c67c6adfec339d78b6e30e
Paweł Szwagierek <szwagier90@gmail.com>
2024-04-25 00:48:46 +0200

    LCD init

--- turbo-water.ino
+++ turbo-water.ino
@@ -1,9 +1,30 @@
+#include "LiquidCrystal_I2C.h"
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // I2C address: 0x27 | LCD: 16x2
+
 void setup() {
-  Serial.begin(9600);
+  delay(100);
+  Serial.begin(115200);
+  delay(100);
   Serial.println("SERIAL INIT");
+
+  lcdInit();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void lcdInit()
+{
+  Serial.println("LCD Initialization");
+
+  lcd.init();
+  lcd.backlight();
+  lcd.print(" PLANT WATERING ");
+  lcd.setCursor(0,1);
+  lcd.print("     SYSTEM     ");
+  delay(2000);
+  lcd.clear();
 }
