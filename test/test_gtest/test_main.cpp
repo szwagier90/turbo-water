120ebd87e9bc5899743721d897229a37c5801450
Paweł Szwagierek <szwagier90@gmail.com>
2025-05-12 01:44:08 +0200

    PIO and GTest project structure

---
+++ test/test_gtest/test_main.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+TEST(DummyTest, gtest_test)
+{
+    EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+	if (RUN_ALL_TESTS())
+	;
+	// Always return zero-code and allow PlatformIO to parse results
+	return 0;
+}
