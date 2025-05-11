f0b1a0fe1feab00544e556e2e1d24cee3e61f4ff
Paweł Szwagierek <szwagier90@gmail.com>
2025-05-12 01:44:08 +0200

    PIO and GTest project structure

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#ifdef ARDUINO
+#include <Arduino.h>
+
+void setup()
+{
+}
+
+void loop()
+{
+}
+
+#else
+// If compiled for native testing, provide an alternative entry point
+#include <iostream>
+
+int main()
+{
+    std::cout << "Running in native environment. No Arduino setup needed.\n";
+    return 0;
+}
+#endif
