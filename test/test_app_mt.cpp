1ded392febfdd0b630609fdd43af1e6b795f6e84
Paweł Szwagierek <szwagier90@gmail.com>
2025-08-08 19:50:11 +0200

    Extract App BasicSetup fixture

--- test/test_app_mt.cpp
+++ test/test_app_mt.cpp
@@ -52,73 +52,58 @@ TEST(AppTest, PeripherialsInitialization)
     app.setup();
 }
 
-TEST(AppTest, DontReadSensorIfNotCalibrated)
+class AppBasicSetupFixture : public ::testing::Test
 {
+protected:
+    void SetUp() override
+    {
+        EXPECT_CALL(serial, begin);
+        EXPECT_CALL(serial, println).Times(2);
+        EXPECT_CALL(lcd, init);
+        EXPECT_CALL(lcd, backlight);
+        EXPECT_CALL(lcd, print).Times(2);
+        EXPECT_CALL(lcd, setCursor);
+        EXPECT_CALL(delay, delay);
+        EXPECT_CALL(lcd, clear);
+        app.setup();
+    }
+
+    void TearDown() override
+    {
+    }
+
     MockSerial serial;
     MockLcd lcd;
     MockDelay delay;
     MockGpio sensorGpio;
     MockGpio pumpGpio;
-    EXPECT_CALL(sensorGpio, pinMode);
-    EXPECT_CALL(sensorGpio, setAnalogReferenceExternal);
-    AnalogInput analogInput(sensorGpio, sensorGpioPin);
-    SoilMoistureSensor s_m_sensor(analogInput);
-    EXPECT_CALL(pumpGpio, pinMode);
-    Pump pump(pumpGpio, pumpGpioPin);
-    App app(
-        serial
-        , lcd
-        , delay
-        , s_m_sensor
-        , pump
-    );
+    AnalogInput analogInput;
+    SoilMoistureSensor s_m_sensor;
+    Pump pump;
+    App app;
 
-    EXPECT_CALL(serial, begin);
-    EXPECT_CALL(serial, println).Times(2);
-    EXPECT_CALL(lcd, init);
-    EXPECT_CALL(lcd, backlight);
-    EXPECT_CALL(lcd, print).Times(2);
-    EXPECT_CALL(lcd, setCursor);
-    EXPECT_CALL(delay, delay);
-    EXPECT_CALL(lcd, clear);
-    app.setup();
+    AppBasicSetupFixture() :
+        analogInput(sensorGpio, sensorGpioPin)
+        , s_m_sensor(analogInput)
+        , pump(pumpGpio, pumpGpioPin)
+        , app(
+            serial
+            , lcd
+            , delay
+            , s_m_sensor
+            , pump
+        ) {};
+};
 
+TEST_F(AppBasicSetupFixture, DontReadSensorIfNotCalibrated)
+{
     EXPECT_CALL(sensorGpio, analogRead).Times(0);
     EXPECT_CALL(pumpGpio, digitalWrite).Times(0);
     app.loop();
 }
 
-TEST(AppTest, ReadSensorIfCalibrated)
+TEST_F(AppBasicSetupFixture, ReadSensorIfCalibrated)
 {
-    MockSerial serial;
-    MockLcd lcd;
-    MockDelay delay;
-    MockGpio sensorGpio;
-    MockGpio pumpGpio;
-    EXPECT_CALL(sensorGpio, pinMode);
-    EXPECT_CALL(sensorGpio, setAnalogReferenceExternal);
-    AnalogInput analogInput(sensorGpio, sensorGpioPin);
-    SoilMoistureSensor s_m_sensor(analogInput);
-    EXPECT_CALL(pumpGpio, pinMode);
-    Pump pump(pumpGpio, pumpGpioPin);
-    App app(
-        serial
-        , lcd
-        , delay
-        , s_m_sensor
-        , pump
-    );
-
-    EXPECT_CALL(serial, begin);
-    EXPECT_CALL(serial, println).Times(2);
-    EXPECT_CALL(lcd, init);
-    EXPECT_CALL(lcd, backlight);
-    EXPECT_CALL(lcd, print).Times(2);
-    EXPECT_CALL(lcd, setCursor);
-    EXPECT_CALL(delay, delay);
-    EXPECT_CALL(lcd, clear);
-    app.setup();
-
     s_m_sensor.calibrate();
     EXPECT_CALL(sensorGpio, analogRead).Times(1);
     EXPECT_CALL(pumpGpio, digitalWrite).Times(1);
